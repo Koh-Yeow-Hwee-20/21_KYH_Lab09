94d4a3c489063aa154d2862790e7c5c19df6f3d7
YeowHwee <yeowhwee13@gmail.com>
2020-11-09 20:33:18 +0800

    yes

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make Character go forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        //Make Character go backward
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, Time.deltaTime * -speed);
+        }
+        //Make Character go right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate (new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+        //Make Character go left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * -rotateSpeed, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/PositionText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionText : MonoBehaviour
+{
+    public GameObject position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position.GetComponent<Text>().text = "position: " +GameObject.FindGameObjectWithTag("Text").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.GetComponent<Text>().text = "position: " + GameObject.FindGameObjectWithTag("Text").transform.position;
+    }
+}
